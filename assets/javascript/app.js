134c826abd60cd43899ae158e103e2c4f654c436
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2017-12-29 20:14:33 -0500

    added on click to animate and stop gifs. it kind of works??

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
+	// array of buttons
 	let topics = ["my hero acadamia", "tokyo ghoul", "akame ga kill", "your lie in april", "erased", "death parade", "noragami", "twin star exorcists", "steins gate", "samurai champloo", "mob psycho 100"];
 
 	
+	// function to add the array to the html in button form
 	function addButtons() {
 
 		for (let i = 0; i < topics.length; i++) {
@@ -23,12 +25,11 @@ $(document).ready(function() {
 	addButtons();
 	
 
-
+	// on click event that searchs fot the button clicked on giphy and adds the image and rating to the html
+	// inner on click event to animate and pause gifs
 	$("button").on("click", function() {
 		
-
 		const search = $(this).attr("data-name");
-		
 
 		const queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         search + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -51,18 +52,44 @@ $(document).ready(function() {
 				const p = $("<p>").text("Rating: " + rating);
 
 				const animeImage = $("<img>");
+				animeImage.addClass("gif");
+
 				animeImage.attr("src", results[i].images.original_still.url);
+
+				
 
 				gifDiv.prepend(p);
 				gifDiv.prepend(animeImage);
 				$("#gif-area").prepend(gifDiv);
 
-			}
 
+
+				$(".gif").on("click", function() {
+
+					let static = animeImage.attr("src", results[i].images.original_still.url)
+
+					let animated = animeImage.attr("src", results[i].images.fixed_height.url)
+
+					if (static) {
+
+						animated
+
+					} else {
+
+						static
+
+					}
+
+				});
+
+			}
 
 		});
 
 	});
+
+
+	
 
 
 
